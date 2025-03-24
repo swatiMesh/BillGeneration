dd17332f0b6be46be0787a3439abc4be6ceca6ab
Swati Meshram <47054595+swatiMesh@users.noreply.github.com>
2025-03-25 00:22:20 +0530

    Create DBConnection.java

---
+++ db/DBConnection.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/billing_db";
+    private static final String USER = "root"; // Use your DB username
+    private static final String PASSWORD = "password"; // Use your DB password
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
